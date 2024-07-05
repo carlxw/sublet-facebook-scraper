10ff38011c2bb2d31388cdf907ed4b0c9543c39d
carlxw <87335263+carlxw@users.noreply.github.com>
2024-07-04 22:01:53 -0400

    Make modifications to logic

--- main.py
+++ main.py
@@ -14,17 +14,33 @@ driver.maximize_window()
 
 
 FB_GROUP_URLS = [
-    {"Rez One/ICON - Student Housing In Waterloo": ["https://www.facebook.com/groups/3428997217345482/"]},
-    {"Student Housing in Waterloo": ["https://www.facebook.com/groups/1998166543836067/"]},
-    {"Rez-One sublets - Student Housing Waterloo": ["https://www.facebook.com/groups/855632539122696/"]},
-    {"Waterloo Student Housing": ["https://www.facebook.com/groups/664699027351008/"]},
-    {"UW/WLU 4 Month Subletting": ["https://www.facebook.com/groups/WaterlooSublet/"]},
+    {"Rez One/ICON - Student Housing In Waterloo": {
+        "buy_and_sell": "https://www.facebook.com/groups/3428997217345482/",
+        "discussion": "https://www.facebook.com/groups/3428997217345482/buy_sell_discussion"
+    }},
+    {"Student Housing in Waterloo": {
+        "buy_and_sell": "https://www.facebook.com/groups/1998166543836067/",
+        "discussion": "https://www.facebook.com/groups/1998166543836067/buy_sell_discussion"
+    }},
+    {"Rez-One sublets - Student Housing Waterloo": {
+        "buy_and_sell": "https://www.facebook.com/groups/855632539122696/",
+        "discussion": "https://www.facebook.com/groups/855632539122696/buy_sell_discussion"
+    }},
+    {"Waterloo Student Housing": {
+        "discussion": "https://www.facebook.com/groups/664699027351008/"
+    }},
+    {"UW/WLU 4 Month Subletting": {
+        "buy_and_sell": "https://www.facebook.com/groups/WaterlooSublet/",
+        "discussion": "https://www.facebook.com/groups/WaterlooSublet/buy_sell_discussion"
+    }},
 ]
 
 
-FEED_CLASS = "x1n2onr6 x1ja2u2z"
+FEED_CLASS = "x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"
 NAME_CLASS = "html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs"
 CONTENT_CLASS = "x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h"
+EMPTY_POST_CLASS = "xod5an3 xq8finb x14vqqas"
+UNAVILABLE_CONTENT_CLASS = "x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xk50ysn xzsf02u x1yc453h"
 FACEBOOK_PROFILE_CLASS = "x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1sur9pj xkrqix3 xzsf02u x1s688f"
 
 
@@ -37,36 +53,38 @@ CHART_DATA = {
 }
 
 
-NUMBER_OF_SCROLLS = 20
+NUMBER_OF_SCROLLS = 1
 
 
 # https://medium.com/elnkart/facebook-login-using-selenium-python-bd28d2cb3740
 def main():  
     for url_entry in FB_GROUP_URLS:
         for group_name, link in url_entry.items():
-            # Add corresponding URL leading to discussions page
-            url_entry[group_name].append(f"{link[0]}buy_sell_discussion/")
-            for url in link:
+            for url in link.values():
                 isBuyAndSell = False if "buy_sell_discussion" in url else True
-                change_url(driver, f"{url}?sorting_setting=CHRONOLOGICAL_LISTINGS" if isBuyAndSell else url)
+                change_url(driver, f"{url}?sorting_setting=CHRONOLOGICAL_LISTINGS" if isBuyAndSell else f"{url}?sorting_setting=RECENT_ACTIVITY")
                 scroll_to_bottom(driver, NUMBER_OF_SCROLLS)
-                soup = BeautifulSoup(driver.page_source, "html.parser")
+                html = driver.page_source
+                soup = BeautifulSoup(html, "html.parser")
                 posts = soup.findAll("div", {"class": FEED_CLASS})
                 names, contents, profiles = [], [], []
-                filter_list = [None, " "]
+                filter_list = [None, " ", "No Content found"]
                 for post in posts:
+                    print(post.find_all(string=True, recursive=False))
+                    print("========================")
+                    print(post.find_all(string=True, recursive=True))
                     # Find the post's OP and content
-                    name = post.find("span", {"class": NAME_CLASS})
+                    name = post.find("span", {"class": NAME_CLASS}, recursive=True)
                     if name is not None:
-                        name = name.findAll(string=True)
-                    content = post.find("span", {"class": CONTENT_CLASS})
+                        name = name.find_all(string=True)
+                    content = post.find("span", {"class": CONTENT_CLASS}, recursive=True)
                     if content is not None:
-                        content = content.findAll(string=True)
+                        content = content.find_all(string=True)
                     if content not in filter_list and name not in filter_list: 
                         names.append(name)
                         contents.append(content)
                         # Find the user profile URL
-                        profile = post.find("a", {"class": FACEBOOK_PROFILE_CLASS})
+                        profile = post.find_all("a", {"class": FACEBOOK_PROFILE_CLASS}, recursive=True)
                         if profile is not None:
                             profile = profile.get("href")
                             profiles.append(profile)
