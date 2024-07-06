69d3ed89469cafbf15c74f487a621231acf0ac4a
carlxw <87335263+carlxw@users.noreply.github.com>
2024-07-06 12:29:26 -0400

    Perfect scraper

--- FacebookScraper.py
+++ FacebookScraper.py
@@ -35,6 +35,7 @@ def add_to_data(names, contents, profiles, data, category, group_name):
 
 
 def generate_excel_file(headers, data):
+    """Generate an Excel file using xlsxwriter API"""
     wb = xlsxwriter.Workbook(f"FACEBOOK_SCRAPED_DATA_{strftime("%Y.%m.%d_%H-%M-%S", gmtime())}.xlsx")
     ws = wb.add_worksheet("Sheet 1")
 
@@ -50,3 +51,33 @@ def generate_excel_file(headers, data):
         ws.write(i + 1, 3, data["content"][i])
 
     wb.close()
+
+
+NAME_CLASS = "html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs"
+CONTENT_CLASS = "x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h"
+FACEBOOK_PROFILE_CLASS = "x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1sur9pj xkrqix3 xzsf02u x1s688f"
+
+
+def scrape(post, names, contents, profiles):
+    """Scrape a post for it's OP, content, and OP URL"""
+    filter_list = [None, " ", "No Content found"]
+    name = post.find("span", {"class": NAME_CLASS}, recursive=True)
+    if name is not None:
+        name = name.find_all(string=True)
+    content = post.find("span", {"class": CONTENT_CLASS}, recursive=True)
+    if content is not None:
+        content = content.find_all(string=True)
+    if content not in filter_list and name not in filter_list: 
+        names.append(name)
+        contents.append(content)
+        profile = post.find("a", {"class": FACEBOOK_PROFILE_CLASS}, recursive=True)
+        if profile is not None:
+            profile = profile.get("href")
+            profiles.append(profile)
+        else:
+            profiles.append("")
+    else:
+        if content not in filter_list and name in filter_list:
+            names.append("UNKNOWN")
+            contents.append(content)
+            profiles.append("")
--- main.py
+++ main.py
@@ -18,30 +18,22 @@ FB_GROUP_URLS = [
         "buy_and_sell": "https://www.facebook.com/groups/3428997217345482/",
         "discussion": "https://www.facebook.com/groups/3428997217345482/buy_sell_discussion"
     }},
-    # {"Student Housing in Waterloo": {
-    #     "buy_and_sell": "https://www.facebook.com/groups/1998166543836067/",
-    #     "discussion": "https://www.facebook.com/groups/1998166543836067/buy_sell_discussion"
-    # }},
-    # {"Rez-One sublets - Student Housing Waterloo": {
-    #     "buy_and_sell": "https://www.facebook.com/groups/855632539122696/",
-    #     "discussion": "https://www.facebook.com/groups/855632539122696/buy_sell_discussion"
-    # }},
-    # {"Waterloo Student Housing": {
-    #     "discussion": "https://www.facebook.com/groups/664699027351008/"
-    # }},
-    # {"UW/WLU 4 Month Subletting": {
-    #     "buy_and_sell": "https://www.facebook.com/groups/WaterlooSublet/",
-    #     "discussion": "https://www.facebook.com/groups/WaterlooSublet/buy_sell_discussion"
-    # }},
+    {"Student Housing in Waterloo": {
+        "buy_and_sell": "https://www.facebook.com/groups/1998166543836067/",
+        "discussion": "https://www.facebook.com/groups/1998166543836067/buy_sell_discussion"
+    }},
+    {"Rez-One sublets - Student Housing Waterloo": {
+        "buy_and_sell": "https://www.facebook.com/groups/855632539122696/",
+        "discussion": "https://www.facebook.com/groups/855632539122696/buy_sell_discussion"
+    }},
+    {"Waterloo Student Housing": {
+        "discussion": "https://www.facebook.com/groups/664699027351008/"
+    }},
+    {"UW/WLU 4 Month Subletting": {
+        "buy_and_sell": "https://www.facebook.com/groups/WaterlooSublet/",
+        "discussion": "https://www.facebook.com/groups/WaterlooSublet/buy_sell_discussion"
+    }},
 ]
-
-
-FEED_CLASS = "x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"
-NAME_CLASS = "html-span xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1hl2dhg x16tdsg8 x1vvkbs"
-CONTENT_CLASS = "x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h"
-EMPTY_POST_CLASS = "xod5an3 xq8finb x14vqqas"
-UNAVILABLE_CONTENT_CLASS = "x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xk50ysn xzsf02u x1yc453h"
-FACEBOOK_PROFILE_CLASS = "x1i10hfl xjbqb8w x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz x1sur9pj xkrqix3 xzsf02u x1s688f"
 
 
 CHART_DATA = {
@@ -53,33 +45,8 @@ CHART_DATA = {
 }
 
 
+FEED_CLASS = "x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"
 NUMBER_OF_SCROLLS = 5
-
-
-def scrape(post, names, contents, profiles):
-    filter_list = [None, " ", "No Content found"]
-    name = post.find("span", {"class": NAME_CLASS}, recursive=True)
-    if name is not None:
-        name = name.find_all(string=True)
-    content = post.find("span", {"class": CONTENT_CLASS}, recursive=True)
-    if content is not None:
-        content = content.find_all(string=True)
-    if content not in filter_list and name not in filter_list: 
-        names.append(name)
-        contents.append(content)
-        profile = post.find("a", {"class": FACEBOOK_PROFILE_CLASS}, recursive=True)
-        if profile is not None:
-            profile = profile.get("href")
-            profiles.append(profile)
-        else:
-            profiles.append("")
-    else:
-        if content not in filter_list and name in filter_list:
-            names.append("UNKNOWN")
-            contents.append(content)
-            profiles.append("")
-
-
 FOUND_POSTS = set()
 
 
@@ -99,18 +66,18 @@ def main():
 
                     # Filter out the bad posts
                     try:
-                        for index, post in enumerate(posts):
-                            print(f"Post number {index} ==============")
-                            print(post.find_all(string=True, recursive=False))
-                            print(post.find_all(string=True, recursive=True))
+                        for _, post in enumerate(posts):
+                            # print(f"Post number {index} ==============")
+                            # print(post.find_all(string=True, recursive=False))
+                            # print(post.find_all(string=True, recursive=True))
+                            if post not in FOUND_POSTS:
+                                new_posts.append(post)
+                                FOUND_POSTS.add(post)
                     except:
                         print("Posts cannot find_all()")
                     
                     # Add new posts to data
                     for post in new_posts:
-                        print(post.find_all(string=True, recursive=False))
-                        print("========================")
-                        print(post.find_all(string=True, recursive=True))
                         scrape(post, names, contents, profiles)
 
                     category = "Buy and Sell" if isBuyAndSell else "Discussion"
